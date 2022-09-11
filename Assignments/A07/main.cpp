85a0026723cc041053cbf9b876e10f482c91e743
Angel Badillo <badilloa022402@gmail.com>
2022-09-11 12:54:12 -0500

    attempting to read data

--- Assignments/A07/main.cpp
+++ Assignments/A07/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <stack>
+#include <vector>
 using namespace std;
-
+#define newl '\n'
 int main()
 {
+    int n;
+    int train;
+    vector<int> trains;
+    stack<int> station;
 
+    while (cin >> n && n){
+        cout << n << newl;
+        while (cin >> train && train)
+        {
+            cout << train << ' ';
+        }
+        cout << newl;
+        return 0;
+    }
 }
