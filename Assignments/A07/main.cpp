abeec777143a354c33d29db8e7bbfab2787f0451
Angel Badillo <badilloa022402@gmail.com>
2022-09-13 21:03:14 -0500

    Worked on Assignment 7, finished handling input

--- Assignments/A07/main.cpp
+++ Assignments/A07/main.cpp
@@ -1,22 +1,49 @@
+/**
+ * @file main.cpp
+ * @author Angel Badillo Hernandez (https://github.com/It-Is-Legend27/)
+ * @brief A solution to the Rails UVA problem.
+ * @date 2022-09-15
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <iostream>
 #include <stack>
 #include <vector>
 using namespace std;
+
 #define newl '\n'
+
 int main()
 {
-    int n;
-    int train;
-    vector<int> trains;
-    stack<int> station;
+    int n;              // Number of coaches
+    int coach;          // ID of coach
+    vector<int> trains; // Vector to hold coaches
+    stack<int> station; // Train station
 
+    // While we can read in # of coaches
     while (cin >> n && n){
-        cout << n << newl;
-        while (cin >> train && train)
+        // While we can read in order of coaches
+        while (cin >> coach && coach)
         {
-            cout << train << ' ';
+            trains.push_back(coach);
+
+            // Read the full line of input
+            while(trains.size() != n)
+            {
+                cin >> coach;
+                trains.push_back(coach);
+            }
+
+            //TODO: INPUT IS HANDLED CORRECTLY. WORK ON LOGIC NOW!
+            
+
+            // Clear vector and stack
+            trains.clear();
+            while(!station.empty())
+            station.pop();
         }
         cout << newl;
-        return 0;
     }
+    return 0;
 }
