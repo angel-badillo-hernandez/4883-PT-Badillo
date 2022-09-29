2929f5997f6926df77901bebc0f3382bf2b7278f
Angel Badillo <badilloa022402@gmail.com>
2022-09-28 19:27:58 -0500

    working on sending email problem

---
+++ Assignments/A10/10986/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file main.cpp
+ * @author Angel Badillo Hernandez (https://github.com/It-Is-Legend27/)
+ * @brief A solution to the Claw Decomposition problem.
+ * @date 2022-09-29
+ *
+ */
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+// Driver program to test above function
+int main()
+{
+    int N, n, m, s, t, w;
+    vector<vector<int>> G;
+    // # cases
+    cin >> N;
+
+
+    while(N)
+    {
+        cin >> n >> m >> s >> t;
+
+    }
+
+    return 0;
+}
