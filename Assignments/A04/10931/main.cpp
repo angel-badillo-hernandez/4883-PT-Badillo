fe7f3d4b07c3f8a2eba89b583837a8db4ea37e77
Angel Badillo <badilloa022402@gmail.com>
2022-10-11 19:44:02 -0500

    working on parity uva

---
+++ Assignments/A04/10931/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Angel Badillo Hernandez (https://github.com/It-Is-Legend27/)
+ * @brief A solution to the Parity UVA problem.
+ * @date 2022-12-02
+ *
+ */
+#include <iostream>
+#include <string>
+#include <bitset>
+using namespace std;
+#define newl '\n'
+
+int main()
+{
+    
+}
