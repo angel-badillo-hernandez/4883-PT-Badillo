f3e53e5e5f50b6f12c80323c6dc9ec00bdc52967
Angel Badillo <badilloa022402@gmail.com>
2022-10-22 17:14:29 -0500

    completed 11172

--- Assignments/A04/11172/main.py
+++ Assignments/A04/11172/main.py
@@ -5,3 +5,15 @@
 `date`: 2022-12-02
 '''
 
+numTestCases: int = int(input())
+
+for t in range(0, numTestCases):
+    # read in input as 2 ints
+    a,b = tuple(map(int,input().split()))
+    res: int = a - b
+    if not res:
+        print('=')
+    if res < 0:
+        print('<')
+    if res > 0:
+        print('>')
